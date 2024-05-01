346bff894d2788222cf18a1a2926c0700c48aea1
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-01 16:07:27 +0200

    feat: added get api list method to OsmfeaturesSyncableTrait and created exceptions class

--- src/Traits/OsmfeaturesSyncableTrait.php
+++ src/Traits/OsmfeaturesSyncableTrait.php
@@ -2,6 +2,52 @@
 
 namespace Wm\WmOsmfeatures\Traits;
 
+use Wm\WmOsmfeatures\Exceptions\WmOsmfeaturesException;
+
 trait OsmfeaturesSyncableTrait
 {
+
+    /**
+     * Get the data from osmfeatures list api url for the model with the given parameters.
+     * @param string|null $updated_at
+     * @param int $page
+     * @param string|null $bbox
+     * @param int|null $score
+     * @param int|null $admin_level
+     * @return string
+     * @throws WmOsmfeaturesException
+     */
+    public function getApiList(string $updated_at = null, int $page = 1, string $bbox = null, int $score = null, int $admin_level = null): string
+    {
+        $apiUrl = $this->osmfeatures_endpoint;
+
+        if (!$apiUrl) {
+            throw WmOsmfeaturesException::missingEndpoint();
+        }
+
+        //build the query
+        $query = [
+            'page' => $page
+        ];
+
+        if ($updated_at) {
+            $query['updated_at'] = $updated_at;
+        }
+
+        if ($bbox) {
+            $query['bbox'] = $bbox;
+        }
+
+        if ($score) {
+            $query['score'] = $score;
+        }
+
+        if ($admin_level) {
+            $query['admin_level'] = $admin_level;
+        }
+
+        $apiUrl .= '?' . http_build_query($query);
+
+        return $apiUrl;
+    }
 }
