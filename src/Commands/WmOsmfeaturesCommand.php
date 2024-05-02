75515c4cec8005d693d80f2fb4f35f1bdea5b502
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-02 09:41:15 +0200

    feat: osmfeatures_sync_command pt1

--- src/Commands/WmOsmfeaturesCommand.php
+++ src/Commands/WmOsmfeaturesCommand.php
@@ -2,18 +2,122 @@
 
 namespace Wm\WmOsmfeatures\Commands;
 
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
+use Wm\WmOsmfeatures\Exceptions\WmOsmfeaturesException;
 
 class WmOsmfeaturesCommand extends Command
 {
-    public $signature = 'wm-osmfeatures';
+    public $signature = 'wm-osmfeatures:sync';
 
-    public $description = 'My command';
+    public $description = 'Begin the OSMFeaturessync process for the initialized models.';
 
-    public function handle(): int
+    public function handle()
     {
-        $this->comment('All done');
+        $models = $this->getInitializedModels();
 
-        return self::SUCCESS;
+        if (empty($models)) {
+            throw WmOsmfeaturesException::missingInitializedModels();
+        }
+
+        foreach ($models as $model) {
+            $table = $this->getTableName($model);
+            $this->initializeTable($table);
+        }
+    }
+
+
+    /**
+     * Return an array of the model's names that implement the OsmfeaturesSyncableTrait
+     * 
+     * @return array
+     */
+
+    protected function getInitializedModels(): array
+    {
+        //get all the models that implement the OsmfeaturesSyncableTrait
+        $trait = 'Wm\WmOsmfeatures\Traits\OsmfeaturesSyncableTrait';
+
+        $modelDirectory = app_path('/Models');
+
+        //get all the files in the model directory
+        $modelFiles = File::files($modelDirectory);
+
+        if (empty($modelFiles)) {
+            throw WmOsmfeaturesException::missingModels();
+        }
+
+        $models = [];
+
+        foreach ($modelFiles as $file) {
+            //get the file content
+            $content = File::get($file);
+
+            //check if the content contains the trait
+            if (Str::contains($content, $trait)) {
+                //get the model name
+                $model = Str::before($file->getFilename(), '.php');
+
+                $models[] = $model;
+            }
+        }
+
+        return $models;
+    }
+
+    /**
+     * Initialize the given database table for osmfeatures sync. Adding osmfeatures_id, osmfeatures_data, osmfeatures_updated_at columns
+     * @param string $table
+     * 
+     * @return void
+     */
+    protected function initializeTable(string $table): void
+    {
+        //get the table schema
+        $schema = DB::getSchemaBuilder();
+
+        //check if the table exists
+        if (!$schema->hasTable($table)) {
+            throw WmOsmfeaturesException::missingTable($table);
+        }
+
+        if ($schema->hasColumns($table, ['osmfeatures_id', 'osmfeatures_data', 'osmfeatures_updated_at'])) {
+            Log::info("Table $table already initialized");
+            $this->info("Table $table already initialized, skipping");
+            return;
+        }
+
+        if (!in_array('osmfeatures_id', $schema->getColumnListing($table))) {
+            DB::statement("ALTER TABLE $table ADD COLUMN osmfeatures_id varchar(255)");
+        }
+
+        if (!in_array('osmfeatures_data', $schema->getColumnListing($table))) {
+            DB::statement("ALTER TABLE $table ADD COLUMN osmfeatures_data jsonb");
+        }
+
+        if (!in_array('osmfeatures_updated_at', $schema->getColumnListing($table))) {
+            DB::statement("ALTER TABLE $table ADD COLUMN osmfeatures_updated_at timestamp");
+        }
+    }
+
+    /**
+     * Get the table name of the given model
+     * @param string $model
+     * 
+     * @return string
+     */
+    protected function getTableName(string $modelName): string
+    {
+        //get an instance of the model class
+        $class = 'App\\Models\\' . $modelName;
+
+        $instance = new $class;
+
+        $table = $instance->getTable();
+
+        return $table;
     }
 }
