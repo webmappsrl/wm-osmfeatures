f870ae91f10e4214d54594bd09e3a91c610e7ec7
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-06 15:27:58 +0200

    feat: created wm osmfeatures import sync command

---
+++ src/Commands/WmOsmfeaturesImportSyncCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Wm\WmOsmfeatures\Commands;
+
+use Illuminate\Console\Command;
+use Wm\WmOsmfeatures\Models\YourModel;
+use Wm\WmOsmfeatures\Traits\OsmfeaturesCommandHelper;
+use Wm\WmOsmfeatures\Exceptions\WmOsmfeaturesException;
+use Wm\WmOsmfeatures\Traits\OsmfeaturesImportableTrait;
+
+class WmosmfeaturesImportSyncCommand extends Command
+{
+    use OsmfeaturesCommandHelper;
+
+
+    protected $signature = 'wm-osmfeatures:import-sync';
+    protected $description = 'Sync all elements from OSM Features to the local database';
+
+    public function handle()
+    {
+        $this->info('Starting import sync process...');
+
+        $models = $this->getInitializedModels('Wm\WmOsmfeatures\Traits\OsmfeaturesImportableTrait');
+
+        if (empty($models)) {
+            throw WmOsmfeaturesException::missingInitializedModels();
+        }
+
+        // Iterate over each model and trigger the import process
+        foreach ($models as $modelName) {
+            $className = $this->getClassName($modelName);
+            $this->info('Starting import for ' . $modelName);
+            $className::importFromOsmFeatures();
+        }
+
+        $this->info('Import sync process completed.');
+    }
+}
