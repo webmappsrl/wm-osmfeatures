8d53083c2d855d7d3b96317195dec01f3c2b11c8
Gennaro Manzo <gennaromanzo1993@gmail.com>
2024-05-08 09:29:28 +0200

    feat: updated wm osmfeatures command to accept model option

--- src/Commands/WmOsmfeaturesCommand.php
+++ src/Commands/WmOsmfeaturesCommand.php
@@ -12,45 +12,69 @@ class WmOsmfeaturesCommand extends Command
 {
     use OsmfeaturesCommandHelper;
 
-    public $signature = 'wm-osmfeatures:sync';
+    public $signature = 'wm-osmfeatures:sync {--model=}';
 
     public $description = 'Begin the OSMFeaturessync process for the initialized models and push sync jobs to the queue.';
 
     public function handle()
     {
-        $this->info('Checking initialized models...');
-        $models = $this->getInitializedModels('Wm\WmOsmfeatures\Traits\OsmfeaturesSyncableTrait');
-
-        if (empty($models)) {
-            throw WmOsmfeaturesException::missingInitializedModels();
-        }
-
-        //for each model initialized with the trait, initialize the table and get all the instances
-        foreach ($models as $modelName) {
-            $this->info('Initializing table for '.$modelName);
-
-            $className = $this->getClassName($modelName);
+        if ($this->option('model')) {
+            $model = $this->option('model');
+            $className = $this->getClassName($model);
             $table = $this->getTableName($className);
 
             $this->initializeTable($table);
             $this->checkFillables($className);
-
-            $this->info('Fetching ids for '.$modelName);
-
+            $this->info('Fetching ids for ' . $model);
             $osmfeaturesIds = $this->fetchOsmfeaturesIds($className);
             if ($osmfeaturesIds->isEmpty()) {
                 throw WmOsmfeaturesException::noOsmfeaturesIdsFound($modelName);
             }
 
-            $this->info('Fetched '.count($osmfeaturesIds).' ids');
-            $this->info('Dispatching jobs for '.$modelName);
+            $this->info('Fetched ' . count($osmfeaturesIds) . ' ids');
+            $this->info('Dispatching jobs for ' . $model);
 
             //dispatch a job for each osmfeatures id
             $osmfeaturesIds->each(function ($osmfeaturesId) use ($className) {
                 dispatch(new OsmfeaturesSyncJob($osmfeaturesId, $className));
             });
-            $this->info("Jobs pushed for $modelName");
-            Log::info("Jobs pushed for $modelName");
+            $this->info("Jobs pushed for $model");
+            Log::info("Jobs pushed for $model");
+        } else {
+            $this->info('Checking initialized models...');
+            $models = $this->getInitializedModels('Wm\WmOsmfeatures\Traits\OsmfeaturesSyncableTrait');
+
+            if (empty($models)) {
+                throw WmOsmfeaturesException::missingInitializedModels();
+            }
+
+            //for each model initialized with the trait, initialize the table and get all the instances
+            foreach ($models as $modelName) {
+                $this->info('Initializing table for ' . $modelName);
+
+                $className = $this->getClassName($modelName);
+                $table = $this->getTableName($className);
+
+                $this->initializeTable($table);
+                $this->checkFillables($className);
+
+                $this->info('Fetching ids for ' . $modelName);
+
+                $osmfeaturesIds = $this->fetchOsmfeaturesIds($className);
+                if ($osmfeaturesIds->isEmpty()) {
+                    throw WmOsmfeaturesException::noOsmfeaturesIdsFound($modelName);
+                }
+
+                $this->info('Fetched ' . count($osmfeaturesIds) . ' ids');
+                $this->info('Dispatching jobs for ' . $modelName);
+
+                //dispatch a job for each osmfeatures id
+                $osmfeaturesIds->each(function ($osmfeaturesId) use ($className) {
+                    dispatch(new OsmfeaturesSyncJob($osmfeaturesId, $className));
+                });
+                $this->info("Jobs pushed for $modelName");
+                Log::info("Jobs pushed for $modelName");
+            }
         }
     }
 }
